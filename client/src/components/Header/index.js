0002d6721a770906368715dd0c0db50e1a9ab813
louislaugier <l_laugier@etu-webschoolfactory.fr>
2020-06-27 20:51:54 +0200

    update

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Header extends React.Component{
+    constructor(props) {
+      super(props)
+      this.state = {}
+    }
+
+    render(){
+      return (
+        <header className='App-header'>
+            header
+        </header>
+      )
+    }
+}
+
+export default Header
--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css'
 import * as serviceWorker from './serviceWorker'
+import Header from './components/Header'
 
 class App extends React.Component{
   constructor(props) {
     super(props)
     this.state = {}
   }
-
+  
   render(){
     return (
       <div className='App'>
-        <header className='App-header'>
-          Header
-        </header>
-        Page
+        <Header/>
       </div>
     )
   }
